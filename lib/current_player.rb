84486f56089f9491ef24098350d758b4063a6267
Alex Braatz <alexjbraatz@gmail.com>
2020-05-12 21:42:46 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,10 +2,11 @@ board = [" "," "," "," "," "," "," "," "," ",]
 
 
 def turn_count(board)
-counter = 0
-board.each do |space|
-  if space == "X" || space == "O"
-    counter += 1
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
   end
 end
 
